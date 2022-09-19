b2ba6d9134083c6c1b732b533e886f9b9cefcbfa
backtofayton <yucelolcay@gmail.com>
2022-09-19 12:35:21 +0300

    offline setup for seeding

--- handlers/status.js
+++ handlers/status.js
@@ -1,7 +1,8 @@
 exports.handler = async (event, context, callback) => {
+  console.log({ env: process.env.DATABASE_URL });
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ up: true })
-  }
-}
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ up: true }),
+  };
+};
